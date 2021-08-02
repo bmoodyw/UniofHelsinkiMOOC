b00f1bad63807dd52348ee90edda07ddf72d17a8
Brian White <bmoodycoding@gmail.com>
2021-08-02 12:20:48 -0700

    completed part 1 different types of input

--- part01-Part01_37.GiftTax/src/main/java/GiftTax.java
+++ part01-Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -2,7 +2,34 @@
 import java.util.Scanner;
 
 public class GiftTax {
+    
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Value of gift?");
+        double input = Double.valueOf(scan.nextLine());
+        
+        double tax1 = (100 + (input - 5000) * 0.08);
+        double tax2 = (1700 + (input - 25000) * 0.10);
+        double tax3 = (4700 + (input - 55000) * 0.12);
+        double tax4 = (22100 + (input - 200000) * 0.15);
+        double tax5 = (142100 + (input - 1000000) * 0.17);
+        
+        if (input < 5000){
+            System.out.println("No tax!");
+        } else if (input >= 5000 && input < 25000) {
+            System.out.println("Tax: " + tax1);
+        } else if (input >=25000 && input < 55000) {
+            System.out.println("Tax: " + tax2);
+        } else if (input >=55000 && input < 200000) {
+            System.out.println("Tax: " +tax3);
+        } else if (input >=200000 && input < 1000000){
+            System.out.println("Tax " +tax4);
+        } else if (input >1000000){
+            System.out.println("Tax: " + tax5);
+        }
 
    
     }
-}==
+}
