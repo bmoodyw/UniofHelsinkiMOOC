77eaeaaff1b2907dcaecc7a6ac54f051f628bdd1
Brian White <bmoodycoding@gmail.com>
2021-08-03 09:27:42 -0700

    first commit

--- part01-Part01_37.GiftTax/src/main/java/GiftTax.java
+++ part01-Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -7,7 +7,7 @@ public class GiftTax {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
-        System.out.println("Value of gift?");
+        System.out.println("Value of the gift?");
         double input = Double.valueOf(scan.nextLine());
         
         double tax1 = (100 + (input - 5000) * 0.08);
@@ -20,11 +20,11 @@ public class GiftTax {
             System.out.println("No tax!");
         } else if (input >= 5000 && input < 25000){
             System.out.println("Tax: " + tax1);
-        } else if (input >= 25000 && input < 55000){
+        } else if (input >= 25000 && input < 55000) {
             System.out.println("Tax: " + tax2);
-        } else if (input >= 55000 && input < 200000){
+        } else if (input >= 55000 && input < 200000) {
             System.out.println("Tax: " + tax3);
-        } else if (input >= 200000 && input < 1000000){
+        } else if (input >= 200000 && input < 1000000) {
             System.out.println("Tax: " + tax4);
         } else if (input > 1000000){
             System.out.println("Tax: " + tax5);
